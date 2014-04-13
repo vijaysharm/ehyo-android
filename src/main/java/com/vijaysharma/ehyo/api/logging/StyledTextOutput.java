3cd0a39ab2fede31acc35ee1848987c773cf2e10
Vijay Sharma <vijay.sharm@gmail.com>
2014-04-13 10:45:48 -0400

    Renamed Outputter

--- src/main/java/com/vijaysharma/ehyo/api/logging/StyledTextOutput.java
+++
@@ -1,7 +0,0 @@
-package com.vijaysharma.ehyo.api.logging;
-
-public interface StyledTextOutput {
-	StyledTextOutput print(Object text);
-	StyledTextOutput println(Object text);
-	StyledTextOutput exception(String message, Throwable throwable);
-}
