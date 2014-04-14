61ea49684e37a7c32551d58b8e5f430323410a7a
Vijay Sharma <vijay.sharm@gmail.com>
2014-04-14 13:21:19 -0400

    Removed unused interfaces

--- src/main/java/com/vijaysharma/ehyo/api/BuildAction.java
+++
@@ -1,6 +0,0 @@
-package com.vijaysharma.ehyo.api;
-
-public interface BuildAction extends PluginAction {
-	void addDependency(BuildConfiguration variant, String dependency);
-	void removeDependency(BuildConfiguration config, String dependency);
-}
--- src/main/java/com/vijaysharma/ehyo/api/ManifestAction.java
+++
@@ -1,6 +0,0 @@
-package com.vijaysharma.ehyo.api;
-
-public interface ManifestAction extends PluginAction {
-	void addPermission(ProjectManifest manifest, String permission);
-	void removePermission(ProjectManifest manifest, String permission);
-}
